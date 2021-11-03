98073c0cb1b7ae516047a95d165527bb8f148664
elias <el4go321@gmail.com>
2021-11-03 12:59:23 -0500

    pagina usuario random

---
+++ pages/usuarios/[userId].tsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+import type { NextPage } from 'next';
+
+const userId: NextPage = () => {
+    return (
+        <div>
+            este es la pagina de user id NN
+        </div>
+    )
+};
+
+export default userId;
+
